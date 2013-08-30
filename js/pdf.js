0dafc9c9e2a3aa736a40815270d6fe0c059d31bc
Liang Shen <shenzhuxi@gmail.com>
2013-08-30 18:29:33 +0100

    update for pdf.js after it started to use pdf.worker.js file

--- js/pdf.js
+++ js/pdf.js
@@ -3,7 +3,6 @@
     attach: function(context, settings) {
       $('.pdf').each(function(){
         var file = $(this).attr('file');
-        console.log($(this).width());
         window.pdfListView = new PDFListView(
           this,
           {
